0cdca56b3a285494db12a265db8ad57e205c6b8b
Bas van 't Hazeveld <basvthazeveld@outlook.com>
2021-12-01 15:04:13 +0100

    Added automatic location in search bar

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -1,14 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+import 'package:geocoder_flutter/geocoder.dart';
 
 class SearchBar extends StatelessWidget {
   final TextEditingController _searchControl = new TextEditingController();
+
+  getCurrentLocationData() async {
+
+    Location location = new Location();
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    LocationData _locationData;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if(!_serviceEnabled){
+      _serviceEnabled = await location.requestService();
+      if(!_serviceEnabled){
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if(_permissionGranted == PermissionStatus.denied){
+      _permissionGranted = await location.requestPermission();
+      if(_permissionGranted != PermissionStatus.granted){
+        return;
+      }
+    }
+
+    _locationData = await location.getLocation();
+    return _locationData;
+
+  }
+
+  getCurrentAddress() async {
+
+    LocationData _currentLocationData;
+
+    _currentLocationData = getCurrentLocationData();
+    final coordinates = new Coordinates(_currentLocationData.latitude, _currentLocationData.longitude);
+    var addresses = await Geocoder.local.findAddressesFromCoordinates(coordinates);
+    var first = addresses.first;
+
+    return first.featureName;
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         color: Colors.blueGrey[50],
-        borderRadius: BorderRadius.all(
+        borderRadius: const BorderRadius.all(
           Radius.circular(5.0),
         ),
       ),
@@ -18,20 +61,20 @@ class SearchBar extends StatelessWidget {
           color: Colors.blueGrey[300],
         ),
         decoration: InputDecoration(
-          contentPadding: EdgeInsets.all(10.0),
+          contentPadding: const EdgeInsets.all(10.0),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(5.0),
-            borderSide: BorderSide(
+            borderSide: const BorderSide(
               color: Colors.white,
             ),
           ),
           enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(
+            borderSide: const BorderSide(
               color: Colors.white,
             ),
             borderRadius: BorderRadius.circular(5.0),
           ),
-          hintText: "Amsterdam, Den Haag",
+          hintText: "${getCurrentAddress()}",
           prefixIcon: Icon(
             Icons.place,
             color: Colors.blueGrey[300],
